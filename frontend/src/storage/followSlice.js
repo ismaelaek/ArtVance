352179ee29ed4d05aac2b5319990385547ae74ad
ismaelaek <ismael.aitelkamel17@gmail.com>
2024-05-19 15:00:34 +0100

    add follow/unfollow logic to feed suggestions

--- frontend/src/storage/followSlice.js
+++ frontend/src/storage/followSlice.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 
-const API_URL = "http://127.0.0.1:8000/api/";
+const API_URL = "http://127.0.0.1:8000/api";
 
 export const followUser = createAsyncThunk(
 	"follow/followUser",
